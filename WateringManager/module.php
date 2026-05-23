034b2a1a94833909e6530e73552cd5dd7743a2c8
Loerdy <gitte@kotira.de>
2026-05-23 14:43:18 +0200

    module.php angelegt

---
+++ WateringManager/module.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+class WateringManager extends IPSModule
+{
+    public function Create()
+    {
+        parent::Create();
+
+        $this->RegisterPropertyInteger('DefaultDuration', 10);
+        $this->RegisterPropertyInteger('PauseBetweenValves', 10);
+        $this->RegisterPropertyInteger('MaxDuration', 60);
+        $this->RegisterPropertyString('Valves', '[]');
+        $this->RegisterPropertyString('Groups', '[]');
+    }
+
+    public function ApplyChanges()
+    {
+        parent::ApplyChanges();
+    }
+
+    public function EmergencyStop()
+    {
+        $this->SendDebug('EmergencyStop', 'Alle Ventile ausschalten wurde ausgelöst.', 0);
+    }
+}
